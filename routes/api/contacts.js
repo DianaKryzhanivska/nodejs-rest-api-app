f8cfdfab1fe54c39ff2aaf02b543e6ef41b451e9
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-11-27 17:12:35 +0200

    small update after code review

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,25 +4,19 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody, isValidId, authenticate } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
-router.get("/", authenticate, ctrl.getAll);
+router.get("/", ctrl.getAll);
 
-router.get("/:id", authenticate, isValidId, ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
+router.delete("/:id", isValidId, ctrl.deleteById);
 
-router.put(
-  "/:id",
-  authenticate,
-  validateBody(schemas.addSchema),
-  isValidId,
-  ctrl.updateById
-);
+router.put("/:id", validateBody(schemas.addSchema), isValidId, ctrl.updateById);
 
 router.patch(
   "/:id/favorite",
